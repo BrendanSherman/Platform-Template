abc110b3bd99cf139e807ccc6be02621a7234b71
natjef <natjef20@bergen.org>
2017-09-16 23:18:55 -0400

    Renamed important files, removed SimpleSlickGame, made exception generator actually refer to the right file, improved file structure. If my suspisons are correct this commit might actually break your library install (in project structure-->libraries). Let me know if that happens, I might have a temporary fix.

--- src/MarioClone.java
+++ src/MarioClone.java
@@ -3,27 +3,26 @@ import java.util.logging.Logger;
 
 import org.newdawn.slick.*;
 import org.newdawn.slick.geom.Rectangle;
-import org.newdawn.slick.geom.Shape;
 
-public class Square extends BasicGame
+public class MarioClone extends BasicGame
 {
-    public Image img;
+    public Image mario;
     private Image bg;
     private int x;
     private int y;
     private Rectangle box = new Rectangle(500, 980, 100, 100);
-    private Rectangle mariocollision;
-    private Music song = new Music("song.ogg");
+    private Rectangle boxHitBox;
+    private Music song = new Music("resources/music/song1.ogg");
 
-    public Square(String gamename, int x, int y) throws SlickException {
+    public MarioClone(String gamename, int x, int y) throws SlickException {
         super(gamename);
         this.x = x;
         this.y = y;
     }
 
     public void init() throws SlickException{
-        img = new Image("images.png");
-        bg = new Image("maxresdefault.jpg");
+        mario = new Image("resources/images/marioFacingRight.png");
+        bg = new Image("resources/images/background1.jpg");
     }
 
     @Override
@@ -51,11 +50,11 @@ public class Square extends BasicGame
         init();
         if(y>=930)
             y = 930;
-        mariocollision = new Rectangle(x, y, 128, 128);
-        if(mariocollision.intersects(box)){
+        boxHitBox = new Rectangle(x, y, 128, 128);
+        if(boxHitBox.intersects(box)){
             x-=5;
         }
-        img.draw(x, y);
+        mario.draw(x, y);
 
     }
 
@@ -70,7 +69,7 @@ public class Square extends BasicGame
         bg.draw(0,0);
         g.setColor(Color.green);
         g.fillRect(0, 1050, 1920, 30);
-        img.draw(x, y);
+        mario.draw(x, y);
         g.setColor(Color.orange);
         g.fillRect(500, 980, 100, 80);
 
@@ -78,20 +77,20 @@ public class Square extends BasicGame
 
     public static void main(String[] args)
     {
-        try
+        try // Creates a new AppContainer and sets resolution, update interval, fullscreen status, and target framerate.
         {
             AppGameContainer appgc;
-            appgc = new AppGameContainer(new Square("Game", 30, 930));
-            appgc.setDisplayMode(1920, 1080, true);
+            appgc = new AppGameContainer(new MarioClone("Game", 30, 930));
+            appgc.setDisplayMode(1920, 1080, false);
             appgc.setMinimumLogicUpdateInterval(5);
             appgc.setMaximumLogicUpdateInterval(5);
             appgc.setTargetFrameRate(60);
             appgc.start();
 
         }
-        catch (SlickException ex)
+        catch (SlickException ex) //Generates a logger if AppContainer fails to start
         {
-            Logger.getLogger(SimpleSlickGame.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(MarioClone.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
