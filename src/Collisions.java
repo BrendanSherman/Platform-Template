4ef483c012ca31ca6e4e78ff67629c5bf7a5dfd6
Brendan Sherman <breshe20@bergen.org>
2017-11-01 12:13:01 -0400

    Fixed collisions detection permanently

--- src/Collisions.java
+++ src/Collisions.java
@@ -1,4 +1,4 @@
-import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Rectangle; //Allows the collisions method to be applicable for rectangles and lines
 
 public interface Collisions {
     public boolean newIntersects(Rectangle r);
